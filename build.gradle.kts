c5d2de94dbf58eb8ae284ed854a6d3936d4226eb
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-08-28 21:18:36 +0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    scala
+}
+
+group = "org.dimitrovchi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang.modules:scala-swing_2.13:2.1.1")
+    implementation("org.scala-lang.modules:scala-java8-compat_2.13:0.9.0")
+    implementation("org.scala-lang.modules:scala-async_2.13:0.10.0")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
